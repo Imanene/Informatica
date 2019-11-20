a16aa1475d0c55181b0a4f55a8c6fa7d91df00d2
Imanene <57986009+Imanene@users.noreply.github.com>
2019-11-20 10:44:10 +0100

    Update Program.cs

--- Hallo/Hallo/Program.cs
+++ Hallo/Hallo/Program.cs
@@ -6,7 +6,7 @@ namespace Hallo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Haii!");
         }
     }
 }
